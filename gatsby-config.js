234e548b9156dac56c64927e7ba7187b33259bb9
learnbi <73713657+learnbi@users.noreply.github.com>
2020-10-31 16:10:36 +0700

    remove ghost-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 let siteConfig;
-let ghostConfig;
 
 // loading site config
 try {
@@ -8,41 +7,17 @@ try {
   siteConfig = null;
 }
 
-// loading ghost config
-try {
-  ghostConfig = require(`./.ghost`);
-} catch (e) {
-  ghostConfig = {
-    development: {
-      apiUrl: process.env.GHOST_API_URL,
-      contentApiKey: process.env.GHOST_CONTENT_API_KEY,
-      version: process.env.GHOST_VERSION,
-    },
-    production: {
-      apiUrl: process.env.GHOST_API_URL,
-      contentApiKey: process.env.GHOST_CONTENT_API_KEY,
-      version: process.env.GHOST_VERSION,
-    },
-  };
-} finally {
-  const { apiUrl, contentApiKey } =
-    process.env.NODE_ENV === `development`
-      ? ghostConfig.development
-      : ghostConfig.production;
-
-  if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
-    ghostConfig = null; //allow default config to take over
-  }
-}
-
 // setting up plugins
 let gatsbyPlugins = [
   {
     resolve: `@draftbox-co/gatsby-ghost-novela-theme`,
     options: {
-      ghostConfig: ghostConfig,
-      siteConfig: siteConfig,
-    },
+      contentPosts: "content/posts",
+      contentAuthors: "content/authors",
+      basePath: "/",
+      sources: {
+        local: true,
+        contentful: false    },
   },
 ];
 
